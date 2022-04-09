cc2b18b64359c1fdb0401e83953da65fb60cb5f0
Suleman Ahmad <87057938+Sulman23@users.noreply.github.com>
2022-04-09 13:32:30 +0500

    Selecting DOM Elements (JS)

---
+++ script.js
@@ -0,0 +1,53 @@
+// Selecting DOM Elements
+
+    // Personal Information
+const fullName = document.getElementById("name");
+const job = document.getElementById("job");
+const image = document.getElementById("image");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const address = document.getElementById("address");
+const city = document.getElementById("city");
+const district = document.getElementById("district");
+const country = document.getElementById("country");
+const github = document.getElementById("github");
+    // Professional Summry
+const summry = document.getElementById("summry");
+    // Skills
+        // React Development
+const reactDev = document.getElementById("reactDev");
+const reactDevBtn = document.getElementById("reactDevBtn");
+
+        // Frontend Development
+const frontendDev = document.getElementById("frontendDev");
+const frontendDevBtn = document.getElementById("frontendDevBtn");
+        // Backend Development
+const backendDev = document.getElementById("backendDev");
+const backendDevBtn = document.getElementById("backendDevBtn");
+        // CMS Development
+const cmsDev = document.getElementById("cmsDev");
+const cmsDevBtn = document.getElementById("cmsDevBtn");
+        // Others
+const others = document.getElementById("others");
+const othersBtn = document.getElementById("othersBtn");
+    // Experience
+
+    // Education
+        // Degree
+const degree = document.getElementById("degree");
+        // Institute
+const institute = document.getElementById("institute");
+        // Starting Year
+const startingYear = document.getElementById("startingYear");
+        // ending Year
+const endingYear = document.getElementById("endingYear");
+        // Adding Education
+const addEdu = document.getElementById("addEdu");
+
+
+
+console.log(fullName);
+
+// Functions
+
+// Event Listeners
